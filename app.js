c87923e75b5c3dbfec8caa9df62b935f4b8d1f27
Andrzej <andrzej.dabrowski94@gmail.com>
2020-10-05 16:57:59 +0200

    AIDA004 controler i pliki widoku

--- app.js
+++ app.js
@@ -5,13 +5,11 @@ const cookieParser = require('cookie-parser');
 const flash = require('connect-flash');
 const routes = require('./routes/index');
 
-
-
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.set(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
